25994c3fbe033d652dd35ea029bee36c12f41b8b
Sebastian Palm <sebastian.palm91@gmail.com>
2018-03-23 14:48:55 +0100

    gitignore

---
+++ public/plugins/plate/src/disable-api.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * This file is part of WordPlate.
+ *
+ * (c) Vincent Klaiber <hello@vinkla.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+remove_action('rest_api_init', 'create_initial_rest_routes', 99);
