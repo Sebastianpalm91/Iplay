66458d038f873bf2597346cacd759e32c77d7304
Sebastian Palm <sebastian.palm91@gmail.com>
2018-04-04 14:24:25 +0200

    fixing home page on styling

--- public/themes/IPLAY/page-templates/Hero.php
+++ public/themes/IPLAY/page-templates/Hero.php
@@ -1,26 +1,35 @@
 <?php /* Template Name: Hero */ ?>
 <?php get_header(); ?>
-<div class="col-md-8 col-md-12 mt-5 mb-5">
-    <?php $fields = get_fields();?>
-    <div class="hero">
-        <p> <?php echo $fields['hero_text']; ?> </p>
-        <img src="<?php echo $fields['hero_image']["url"]; ?>" alt="" height="50%" width="50%">
-    </div>
+<div class="col-md-12 p-0 m-0">
+    <div class="home-container">
+        <?php $fields = get_fields();?>
+        <div class="hero">
+            <div class="hero-image">
+                <p class="hero-text"> <?php echo $fields['hero_text']; ?> </p>
+                <img src="<?php echo $fields['hero_image']["url"]; ?>" alt="">
+            </div>
+        </div>
 
-    <div class="uniting-sport">
-        <p> <?php echo $fields['uniting_text'] ?></p>
-        <img src="<?php echo $fields['uniting_image']["url"]; ?>" alt="" height="50%" width="50%">
-    </div>
+        <div class="uniting-sport">
+            <p class="uniting-text"> <?php echo $fields['uniting_text'] ?></p>
+            <div class="uniting-image">
+                <img  src="<?php echo $fields['uniting_image']["url"]; ?>" alt="">
+            </div>
+        </div>
 
-    <div class="app-tour">
-        <p> <?php echo $fields['app_text'] ?></p>
-        <img src="<?php echo $fields['app_image']["url"]; ?>" alt="" height="50%" width="50%">
-    </div>
+        <div class="app-tour">
+            <p class="app-text"> <?php echo $fields['app_text'] ?></p>
+            <div class="app-image">
+                <img src="<?php echo $fields['app_image']["url"]; ?>" alt="">
+            </div>
+        </div>
 
-    <div class="news-feed">
-        <p> <?php echo $fields['news_text'] ?></p>
-        <img src="<?php echo $fields['news_image']["url"]; ?>" alt="" height="50%" width="50%">
+        <div class="news-feed">
+            <p class="news-text"> <?php echo $fields['news_text'] ?></p>
+            <div class="news-image">
+                <img src="<?php echo $fields['news_image']["url"]; ?>" alt="" >
+            </div>
+        </div>
     </div>
-
 </div>
 <?php get_footer();
