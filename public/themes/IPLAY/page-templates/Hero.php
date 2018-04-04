0a4a071a6b2ab3bc7c49e23c0b65650920ea1b9c
Sebastian Palm <sebastian.palm91@gmail.com>
2018-04-04 10:42:06 +0200

    custom fields and working in home page

--- public/themes/IPLAY/page-templates/Hero.php
+++ public/themes/IPLAY/page-templates/Hero.php
@@ -1,10 +1,26 @@
 <?php /* Template Name: Hero */ ?>
 <?php get_header(); ?>
-<div class="col-md-8 mt-5 mb-5">
-    <?php //$fields = get_fields();
-            //$fields['hero_text'];
-            ?>
-    <?php the_field("hero_text"); ?>
-    <img src="<?php echo get_field("hero_image")["url"]; ?>" alt="" height="50%" width="50%">
+<div class="col-md-8 col-md-12 mt-5 mb-5">
+    <?php $fields = get_fields();?>
+    <div class="hero">
+        <p> <?php echo $fields['hero_text']; ?> </p>
+        <img src="<?php echo $fields['hero_image']["url"]; ?>" alt="" height="50%" width="50%">
+    </div>
+
+    <div class="uniting-sport">
+        <p> <?php echo $fields['uniting_text'] ?></p>
+        <img src="<?php echo $fields['uniting_image']["url"]; ?>" alt="" height="50%" width="50%">
+    </div>
+
+    <div class="app-tour">
+        <p> <?php echo $fields['app_text'] ?></p>
+        <img src="<?php echo $fields['app_image']["url"]; ?>" alt="" height="50%" width="50%">
+    </div>
+
+    <div class="news-feed">
+        <p> <?php echo $fields['news_text'] ?></p>
+        <img src="<?php echo $fields['news_image']["url"]; ?>" alt="" height="50%" width="50%">
+    </div>
+
 </div>
 <?php get_footer();
