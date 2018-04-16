2f9788f199d6d9118f52ace4bff3e0a53a54f69d
Sebastian Palm <sebastian.palm91@gmail.com>
2018-04-16 17:11:08 +0200

    working on SPORTS page response and final sub

--- public/themes/IPLAY/page-templates/Sports.php
+++ public/themes/IPLAY/page-templates/Sports.php
@@ -24,6 +24,74 @@
             </div>
         </div>
     </div>
+    <div class="sportsoniplay-container">
+        <div class="iplaysports-text-wrapper">
+            <p class="iplaysports-text">Sports on IPLAY</p>
+        </div>
+
+        <div class="iplaysports-sub-text-wrapper">
+            <p class="iplaysports-sub-text">Avaliable right now:</p>
+        </div>
+
+        <div class="sports-icon-avaliable-wrapper">
+            <div class="football-container">
+                <img class="sport-football-picture" src="<?php echo $fields['sports_icons_football']["url"]; ?>" alt="">
+                <div class="football-icon-text"><p>Fotball</p></div>
+            </div>
+
+            <div class="handball-container">
+                <img class="sport-handball-picture" src="<?php echo $fields['sports_icons_handball']["url"]; ?>" alt="">
+                <div class="handball-icon-text"><p>Handball</p></div>
+            </div>
+
+            <div class="bandy-container">
+                <img class="sport-bandy-picture" src="<?php echo $fields['sports_icons_bandy']["url"]; ?>" alt="">
+                <div class="bandy-icon-text"><p>Bandy</p></div>
+            </div>
+
+            <div class="basketball-container">
+                <img class="sport-basketball-picture" src="<?php echo $fields['sports_icons_basketball']["url"]; ?>" alt="">
+                <div class="basketball-icon-text"><p>Basketball</p></div>
+            </div>
+        </div>
+
+        <div class="coming-soon">
+            <p><strong>Coming soon:</strong></p>
+        </div>
+
+        <div class="sports-icon-coming-wrapper">
+
+                    <div class="sport5">
+                        <img class="sport-hockey-picture" src="<?php echo $fields['sports_icons_hockey']["url"]; ?>" alt="">
+                        <div class="sport5-text"><p>Hockey</p></div>
+                    </div>
+                    <div class="sport6">
+                        <img class="sport-esports-picture" src="<?php echo $fields['sports_icons_e-sports']["url"]; ?>" alt="">
+                        <div class="sport6-text"><p>E-sports</p></div>
+                    </div>
+
+                    <div class="sport7">
+                        <img class="sport-skiing-picture" src="<?php echo $fields['sports_icons_skiing']["url"]; ?>" alt="">
+                        <div class="sport7-text"><p>E-Skiing</p></div>
+                    </div>
+                    <div class="sport8">
+                        <img class="sport-floorball-picture" src="<?php echo $fields['sports_icons_floorball']["url"]; ?>" alt="">
+                        <div class="sport8-text"><p>Floorball</p></div>
+                    </div>
+
+                    <div class="sport9">
+                        <img class="sport-horsesports-picture" src="<?php echo $fields['sports_icons_horsesports']["url"]; ?>" alt="">
+                        <div class="sport9-text"><p>Horse Sports</p></div>
+                    </div>
+                    <div class="sport10">
+                        <img class="sport-sailing-picture" src="<?php echo $fields['sports_icons_sailing']["url"]; ?>" alt="">
+                        <div class="sport10-text"><p>Sailing</p></div>
+                    </div>
+
+        </div>
+
+    </div>
+
 
 
     <?php get_footer(); ?>
--- resources/assets/scripts/home/_navscroll.js
+++ resources/assets/scripts/home/_navscroll.js
@@ -1,7 +1,4 @@
 const nav = document.querySelector('.fixed-position');
-
-const hamburger = document.querySelector('.hamburger-inner');
-
 const twitter = document.querySelector('.twitter');
 const instagram = document.querySelector('.instagram');
 const facebook = document.querySelector('.facebook');
@@ -11,12 +8,11 @@ cont.addEventListener('scroll', () => {
   let scroll = cont.scrollTop;
   if (scroll >= 50) {
     nav.classList.add('menu-container');
+    // NAVIGATION MENU BLACK
     const icon = document.querySelectorAll('.navigation-bar ul li a');
-    console.log(icon);
     icon.forEach((e) => {
         e.classList.add('nav-color-scroll');
     })
-    hamburger.classList.add('hamburger-color-scroll');
     // ICONS WHITE
     twitter.classList.remove('twitter');
     instagram.classList.remove('instagram');
@@ -28,17 +24,17 @@ cont.addEventListener('scroll', () => {
     facebook.classList.add('facebook-black');
     youtube.classList.add('youtube-black');
   } else if (scroll < 50 ){
+    nav.classList.remove('menu-container');
+    // NAVIGATION MENU WHITE
       const icon = document.querySelectorAll('.navigation-bar ul li a');
       icon.forEach((e) => {
           e.classList.remove('nav-color-scroll');
       })
-    hamburger.classList.remove('hamburger-color-scroll');
     // ICONS WHITE
     twitter.classList.add('twitter');
     instagram.classList.add('instagram');
     facebook.classList.add('facebook');
     youtube.classList.add('youtube');
-    nav.classList.remove('menu-container');
     // ICONS BLACK
     twitter.classList.remove('twitter-black');
     instagram.classList.remove('instagram-black');
